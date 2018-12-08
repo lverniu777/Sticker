c2e45c56b738951030d595eb37e0b09abb06e9a6
fubiwei <lverniu777@163.com>
2018-12-08 14:11:07 +0800

    获得焦点才绘制边框及删除按钮

--- app/src/main/java/com/example/junyizhou/imagehandledemo/view/StickerView.java
+++ app/src/main/java/com/example/junyizhou/imagehandledemo/view/StickerView.java
@@ -21,6 +21,9 @@ public class StickerView extends BaseView {
      * 添加进来的贴纸容器
      */
     private final List<Sticker> mStickerList = new ArrayList<>();
+    /**
+     * 画贴纸的边框和删除按钮的圆形框
+     */
     private final Paint mPaintForLineAndCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     {
@@ -29,14 +32,23 @@ public class StickerView extends BaseView {
         mPaintForLineAndCircle.setAlpha(170);
     }
 
-    private int moveTag = 0, transformTag = 0, deleteTag = 0;
-
-    private Bitmap deleteIcon;
+    /**
+     * 当前选中或者是作用的贴纸索引
+     */
+    private int mCurrentStickerIndex = 0;
+    /**
+     * 当前删除的贴纸索引
+     */
+    private int mCurrentDeleteIndex = 0;
+    /**
+     * 删除小叉子按钮
+     */
+    private Bitmap mDeleteIcon;
 
 
     public StickerView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
-        deleteIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_close);
+        mDeleteIcon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_close);
     }
 
     @Override
@@ -64,22 +76,30 @@ public class StickerView extends BaseView {
             final float y3 = points[5];
             final float x4 = points[6];
             final float y4 = points[7];
-            drawBounds(canvas, x1, y1, x2, y2, x3, y3, x4, y4);
-            drawDeleteBtn(canvas, x2, y2);
+            //当前绘制的贴纸是不是获取了焦点
+            final boolean isCurrentFocusSticker = i == mCurrentStickerIndex;
+            drawBounds(canvas, x1, y1, x2, y2, x3, y3, x4, y4, isCurrentFocusSticker);
+            drawDeleteBtn(canvas, x2, y2, isCurrentFocusSticker);
             drawStickerBitmap(canvas, sticker);
         }
     }
 
-    private void drawBounds(Canvas canvas, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+    private void drawBounds(Canvas canvas, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, boolean hasFocused) {
+        if (!hasFocused) {
+            return;
+        }
         canvas.drawLine(x1, y1, x2, y2, mPaintForLineAndCircle);
         canvas.drawLine(x2, y2, x4, y4, mPaintForLineAndCircle);
         canvas.drawLine(x4, y4, x3, y3, mPaintForLineAndCircle);
         canvas.drawLine(x3, y3, x1, y1, mPaintForLineAndCircle);
     }
 
-    private void drawDeleteBtn(Canvas canvas, float x2, float y2) {
+    private void drawDeleteBtn(Canvas canvas, float x2, float y2, boolean hasFocused) {
+        if (!hasFocused) {
+            return;
+        }
         canvas.drawCircle(x2, y2, 40, mPaintForLineAndCircle);
-        canvas.drawBitmap(deleteIcon, x2 - deleteIcon.getWidth() / 2, y2 - deleteIcon.getHeight() / 2, mPaintForBitmap);
+        canvas.drawBitmap(mDeleteIcon, x2 - mDeleteIcon.getWidth() / 2, y2 - mDeleteIcon.getHeight() / 2, mPaintForBitmap);
     }
 
     private void drawStickerBitmap(Canvas canvas, Sticker sticker) {
@@ -91,54 +111,61 @@ public class StickerView extends BaseView {
             case MotionEvent.ACTION_DOWN:
                 anchorX = event.getX();
                 anchorY = event.getY();
-                moveTag = decalCheck(anchorX, anchorY);
-                deleteTag = deleteCheck(anchorX, anchorY);
-                if (moveTag != -1 && deleteTag == -1) {
-                    downMatrix.set(mStickerList.get(moveTag).matrix);
+                updateFocusedStickerIndex(stickerCheck(anchorX, anchorY));
+                mCurrentDeleteIndex = deleteCheck(anchorX, anchorY);
+                if (mCurrentStickerIndex != -1 && mCurrentDeleteIndex == -1) {
+                    downMatrix.set(mStickerList.get(mCurrentStickerIndex).matrix);
                     mode = DRAG;
+                    invalidate();
                 }
                 break;
 
             case MotionEvent.ACTION_POINTER_DOWN:
-                moveTag = decalCheck(event.getX(0), event.getY(0));
-                transformTag = decalCheck(event.getX(1), event.getY(1));
-                if (moveTag != -1 && transformTag == moveTag && deleteTag == -1) {
-                    downMatrix.set(mStickerList.get(moveTag).matrix);
+                mCurrentStickerIndex = stickerCheck(event.getX(0), event.getY(0));
+                final int transformTag = stickerCheck(event.getX(1), event.getY(1));
+                if (mCurrentStickerIndex != -1 && transformTag == mCurrentStickerIndex && mCurrentDeleteIndex == -1) {
+                    downMatrix.set(mStickerList.get(mCurrentStickerIndex).matrix);
                     mode = ZOOM;
                 }
                 oldDistance = getDistance(event);
                 oldRotation = getRotation(event);
-
                 midPoint = midPoint(event);
                 break;
 
             case MotionEvent.ACTION_MOVE:
+                if (mCurrentStickerIndex <= -1 || mCurrentStickerIndex >= mStickerList.size()) {
+                    return true;
+                }
+                final Sticker sticker = mStickerList.get(mCurrentStickerIndex);
                 if (mode == ZOOM) {
                     moveMatrix.set(downMatrix);
                     float newRotation = getRotation(event) - oldRotation;
                     float newDistance = getDistance(event);
                     float scale = newDistance / oldDistance;
-                    moveMatrix.postScale(scale, scale, midPoint.x, midPoint.y);// 縮放
-                    moveMatrix.postRotate(newRotation, midPoint.x, midPoint.y);// 旋轉
-                    if (moveTag != -1) {
-                        mStickerList.get(moveTag).matrix.set(moveMatrix);
-                    }
+                    //缩放
+                    moveMatrix.postScale(scale, scale, midPoint.x, midPoint.y);
+                    //旋转
+                    moveMatrix.postRotate(newRotation, midPoint.x, midPoint.y);
+                    sticker.matrix.set(moveMatrix);
                     invalidate();
                 } else if (mode == DRAG) {
                     moveMatrix.set(downMatrix);
-                    moveMatrix.postTranslate(event.getX() - anchorX, event.getY() - anchorY);// 平移
-                    if (moveTag != -1) {
-                        mStickerList.get(moveTag).matrix.set(moveMatrix);
-                    }
+                    //进行平移操作
+                    moveMatrix.postTranslate(event.getX() - anchorX, event.getY() - anchorY);
+                    mStickerList.get(mCurrentStickerIndex).matrix.set(moveMatrix);
                     invalidate();
                 }
                 break;
 
             case MotionEvent.ACTION_UP:
-                if (deleteTag != -1) {
-                    mStickerList.remove(deleteTag).release();
-                    invalidate();
+                if (mCurrentDeleteIndex <= -1 || mCurrentDeleteIndex >= mStickerList.size()) {
+                    return true;
                 }
+                final Sticker stickerToBeRemoved;
+                if ((stickerToBeRemoved = mStickerList.remove(mCurrentDeleteIndex)) != null) {
+                    stickerToBeRemoved.release();
+                }
+                invalidate();
                 mode = NONE;
                 break;
 
@@ -192,7 +219,7 @@ public class StickerView extends BaseView {
         return -1;
     }
 
-    private int decalCheck(float x, float y) {
+    private int stickerCheck(float x, float y) {
         for (int i = 0; i < mStickerList.size(); i++) {
             if (pointCheck(mStickerList.get(i), x, y)) {
                 return i;
@@ -217,7 +244,12 @@ public class StickerView extends BaseView {
         imageGroupTemp.matrix.postTranslate(transX, transY);
         imageGroupTemp.matrix.postScale(0.5f, 0.5f, getWidth() / 2, getHeight() / 2);
         mStickerList.add(imageGroupTemp);
-
+        //最新添加的贴纸即为当前获得焦点的贴纸
+        updateFocusedStickerIndex(mStickerList.size() - 1);
         invalidate();
+    }
+
+    private void updateFocusedStickerIndex(int i) {
+        mCurrentStickerIndex = i;
     }
 }
